84d69e956bb860470363ecc9913d9e8942fd380c
梁家彰 <508895166@qq.com>
2019-11-21 09:42:28 +0800

    修改db配置，使用pymysql代替mysqldb

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 #导入flask模块
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+import config
 
 #导入注册的蓝图
 from api.testHello import testHello
@@ -7,11 +9,17 @@ from api.testGoodBye import testGoodBye
 
 #定义app
 app = Flask(__name__)
+app.config.from_object(config)
 
 #注册蓝图到app
 app.register_blueprint(testHello)
 app.register_blueprint(testGoodBye)
 
+db = SQLAlchemy(app)
+
+# class Country(db.Model)
+
 #程序入口
 if __name__ == '__main__':
+    print(db)
     app.run()

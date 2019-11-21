355e88e878b3ce3ed58008141539ca5a55f9cb7c
梁家彰 <508895166@qq.com>
2019-11-21 11:09:31 +0800

    添加文件日志处理器，并测试日志输出

--- api/testHello.py
+++ api/testHello.py
@@ -1,6 +1,13 @@
 from api import testHello
+from flask import current_app   #日志使用 (flask使用的是python标准的logger)
 
 
 @testHello.route('/')
 def hello():
+    current_app.logger.info('hello world logging test info!')
+    current_app.logger.warning('warning logger!')
+    current_app.logger.error('error logger!')
+    # current_app.logger.exception('exception logger!')
+    # current_app.logger.critical('critical logger!')
+    # current_app.logger.debug('debug logger!')
     return 'Hello World'
--- app.py
+++ app.py
@@ -1,11 +1,20 @@
 #导入flask模块
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-import config
+import config   #数据库配置
+import logging  #日志配置
+from logging.handlers import RotatingFileHandler
 
 #导入注册的蓝图
 from api.testHello import testHello
 from api.testGoodBye import testGoodBye
+
+#日志配置参数
+log_path = './logs/engine.log'
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')   #格式化
+file_handler = RotatingFileHandler(log_path, maxBytes=10*1024*1024, backupCount=10)   #文件切割大小，按字节计算，10个文件后覆盖之前的
+file_handler.setFormatter(formatter)
+file_handler.setLevel(logging.INFO)     #日志等级
 
 #定义app
 app = Flask(__name__)
@@ -15,11 +24,16 @@ app.config.from_object(config)
 app.register_blueprint(testHello)
 app.register_blueprint(testGoodBye)
 
+#注册数据库连接
 db = SQLAlchemy(app)
+
+#为app程序添加 文件日志处理器 默认是stream out handler
+app.logger.addHandler(file_handler)
+app.logger.setLevel(logging.INFO)
 
 # class Country(db.Model)
 
 #程序入口
 if __name__ == '__main__':
-    print(db)
+    # print(db)
     app.run()
